1d67886dd294156ee2c6b437d6b9f667649155f3
michaelagarwal2357 <michaelagarwalDM@gmail.com>
2015-01-25 15:03:33 -0600

    Version 7

--- CodeBook.md
+++ CodeBook.md
@@ -1,4 +1,16 @@
 ### Code book for Getting and cleaning data collected from the accelerometers
+### Author: Michael Agarwal (https://github.com/michaelagarwal/ProjectForGettingAndCleaningData)
+### Date Written: 01/23/2015
+
+### Variables and data
+Data from accelerometers: There are 10299 observations (561 columns), they are listed in X_train.txt and X_test.txt
+Features (i.e column names): There are 561 values, they are listed in Features.txt
+Activity Description: Activity description (6 values) are stored in activity_labels.txt
+Activity for each observation: Activity for each of 10299 observations is stored in single column in Y_train.txt and Y_test.txt
+Subjects for each observation: Subjects for each of 10299 observations is stored in subjects_test.txt and subjects_train_txt
+
+
+### Logic and transformations used in this run_analysis.R is
 
 #### 1. Download data from here
 http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
@@ -12,3 +24,11 @@ http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartpho
 #### 5. Read columns names from features.txt and assign them to 561 columns
 
 #### 6. Filter out columns which are not needed and only keep columns which have names like mean and std
+
+#### 7. Read activity labels and match them with activity listed in Y_train.txt and Y_test.txt
+
+#### 8. Read subjects for each of 10299 obseervations
+
+#### 9. Merge #6, #7 and #8 to have subjects, activity and observations into single data frame
+
+#### 10. Get mean of all observations grouped by subjects and activity and exported it into text file
--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 ##################################################
-# This program is for project work for Getting and Cleaning Data
+# This program is for project work done for Getting and Cleaning Data
 # Author: Michael Agarwal
 # Date Written: 01/24/2015
 ##################################################
@@ -12,6 +12,8 @@ X_test <- read.table("../X_test.txt")
 X_all <- rbind(X_train,X_test)
 ##################################################
 #   Step 2: Extracts only the measurements on the mean and standard deviation for each measurement
+#   AND
+#   Step 4: Appropriately labels the data set with descriptive variable names. 
 #   Get column names from features.txt 
 X_column_names_table <- read.table("../features.txt")
 #   Set column names
@@ -20,9 +22,32 @@ setnames(X_all,as.character(X_column_names_table$V2))
 #   remove columns which does not end with -std() or -mean()
 X_all_subset_1 <- X_all[ , which(names(X_all) %like% "-mean()")]
 X_all_subset_2 <- X_all[ , which(names(X_all) %like% "-std()")]
-X_all_subset <- cbind(X_all_subset_1, X_all_subset_2)
+#   This following data set will be used for final tidy data for measurements
+Data_set_final <- cbind(X_all_subset_1, X_all_subset_2)
 ###################################################
 #   Step 3: Uses descriptive activity names to name the activities in the data set
-    Y_train <- read.table("../Y_train.txt")
-    Y_test <- read.table("../Y_test.txt")
-View(Y_test)
+Y_train <- read.table("../Y_train.txt")
+Y_test <- read.table("../Y_test.txt")
+Activity <- read.table("../activity_labels.txt")
+Y_train_new <- merge(Y_train, Activity, by="V1", sort = 'F')
+Y_test_new <- merge(Y_test, Activity, by="V1", sort = 'F')  
+#   This following data set will be used for final tidy data for activity
+Activity_final <- rbind(Y_train_new,Y_test_new)
+###################################################
+#   Step 4: Appropriately labels the data set with descriptive variable names. 
+#   Step 4 was done with Step 2.
+##################################################
+#   Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+Subject_test <- read.table("../subject_test.txt")
+Subject_train <- read.table("../subject_train.txt")
+#   This following data set will be used for final tidy set for subjects
+Subject_final <- rbind(Subject_train,Subject_test)
+# Put all data together in one dataset and give meaningful names
+All_final <- cbind(Subject_final$V1,Activity_final$V2,Data_set_final)
+colnames(All_final)[1]<-"Subject"
+colnames(All_final)[2]<-"Activity"
+# Prepare tidy set and export it into text file
+All_final <- as.data.table(All_final)
+variables <- tail( names(All_final), -5)
+Tidy_data <- All_final[, lapply(.SD, mean), .SDcols=variables, by=list(Subject, Activity)]
+write.table(Tidy_data, file="Tidy_data_set.txt",row.name=FALSE)
